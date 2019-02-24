25deb461f8f3080eda42f22878103418808ebcb1
Ahmet Atahan Mutlu <39701542+aatahanm@users.noreply.github.com>
2019-02-24 03:21:28 +0300

    updated map

--- java/rokettakimi/mug2019/MapActivity.java
+++ java/rokettakimi/mug2019/MapActivity.java
@@ -9,12 +9,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MapActivity extends AppCompatActivity {
 
     private boolean commanderSelected = false;
     private boolean conqueredGaul = false;
+    private Animation fadeOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,16 @@ public class MapActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.map_activity_main);
+        setContentView(R.layout.map_activity);
+
+        TextView eventTitle = findViewById(R.id.title);
+
+        fadeOut = new AlphaAnimation(1.0f , 0.0f);
+        fadeOut.setDuration(9000);
+        fadeOut.setFillAfter(true);
+
+        eventTitle.startAnimation(fadeOut);
+
     }
 
     public void commanderSelected(View v){
@@ -31,6 +45,9 @@ public class MapActivity extends AppCompatActivity {
         if(!conqueredGaul) {
             setMovable(R.id.gallicCity1Button);
             setMovable(R.id.legion1);
+        }else {
+            setMovable(R.id.gallicCity1Button);
+            setMovable(R.id.romeCityButton);
         }
 
     }
@@ -40,10 +57,17 @@ public class MapActivity extends AppCompatActivity {
 
         switch (id) {
             case R.id.gallicCity1Button:
-                img.setImageResource(R.drawable.gallic_movable);
+                if(!conqueredGaul)
+                    img.setImageResource(R.drawable.gallic_movable);
+                else
+                    img.setImageResource(R.drawable.city_movable);
                 break;
             case R.id.legion1:
                 img.setImageResource(R.drawable.helmet_movable);
+                break;
+            case R.id.romeCityButton:
+                img.setImageResource(R.drawable.city_movable);
+                break;
         }
     }
 
@@ -72,22 +96,43 @@ public class MapActivity extends AppCompatActivity {
         }
     }
 
-    public void moveCaesarToGaul(View view){
-        if(commanderSelected) {
+    public void movetoRoma(View view){
+        if(commanderSelected && conqueredGaul){
             View v = findViewById(R.id.caesarModel);
 
+            moveTo(640f, 330f, 500, 0, v);
+            moveTo(1100f, 200f, 1500, 500, v);
+            moveTo(1280f,415f,1500,2000,v);
+
+        }else
+        {
+            showCityInfo(view);
+        }
+    }
+
+    public void showCityInfo(View v){
+        Toast.makeText(getApplicationContext(),"city info",Toast.LENGTH_LONG).show();
+        //Show city info
+    }
+
+    public void moveCaesarToGaul(View view){
+        if(commanderSelected) {
+
+            View v = findViewById(R.id.caesarModel);
             conquerGaul(1);
 
             moveTo(1100f, 200f, 1500, 0, v);
             moveTo(640f, 330f, 1500, 1500, v);
             moveTo(640f, 300f, 500, 3000, v);
+        }else{
+            showCityInfo(view);
         }
 
     }
 
     public void conquerGaul(int count){
         View v = findViewById(R.id.background);
-        while (count < 7){
+        while (count < 8 && !conqueredGaul){
             if(count == 1){
                 setImage(v,R.drawable.gaul_siege1,3500+count*1500);
             }else if (count == 2){
@@ -101,6 +146,8 @@ public class MapActivity extends AppCompatActivity {
                 setImage(findViewById(R.id.gallicCity2Button),R.drawable.rome_city3,3500+count*1500);
             }else if (count == 6){
                 setImage(v,R.drawable.gaul_siege4,3500+count*1500);
+            }else if (count == 7){
+                setImage(findViewById(R.id.romeCityButton),R.drawable.city_movable,3500+count*1500);
             }
             count+=1;
         }
@@ -141,10 +188,16 @@ public class MapActivity extends AppCompatActivity {
         ImageView img = findViewById(R.id.gallicCity1Button);
         if(!conqueredGaul) {
             img.setImageResource(R.drawable.gallic);
+        }else{
+            img.setImageResource(R.drawable.rome_city2);
         }
+
 
         img= findViewById(R.id.legion1);
         img.setImageResource(R.drawable.helmet);
+
+        img = findViewById(R.id.romeCityButton);
+        img.setImageResource(R.drawable.rome2);
 
     }
 }
